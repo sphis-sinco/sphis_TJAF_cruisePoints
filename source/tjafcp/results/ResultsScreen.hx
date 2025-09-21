8c9ae6ebbfbe2a584565642f156514a806e48bc8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-20 21:30:52 -0500

    ResultsScreen characterManager

--- source/tjafcp/results/ResultsScreen.hx
+++ source/tjafcp/results/ResultsScreen.hx
@@ -1,5 +1,16 @@
 package tjafcp.results;
 
 import flixel.FlxState;
+import tjafcp.results.ResultsCharacter.ResultsCharacterID;
 
-class ResultsScreen extends FlxState {}
+class ResultsScreen extends FlxState
+{
+	public var characterManager:ResultsCharacter;
+
+	public override function new(?character:ResultsCharacterID = NONE)
+	{
+		super();
+
+		characterManager = new ResultsCharacter(character);
+	}
+}
