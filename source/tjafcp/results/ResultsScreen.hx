1ef26a1d62f55cae489ea2e0a18d9f08bdb858b0
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-20 21:52:40 -0500

    bgColor

--- source/tjafcp/results/ResultsScreen.hx
+++ source/tjafcp/results/ResultsScreen.hx
@@ -1,6 +1,9 @@
 package tjafcp.results;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import haxe.macro.Compiler;
 import tjafcp.results.ResultsCharacter.ResultsCharacterID;
 
 class ResultsScreen extends FlxState
@@ -21,5 +24,20 @@ class ResultsScreen extends FlxState
 
 		score = new ResultsScore(stats.good, stats.total);
 		characterManager = new ResultsCharacter(character);
+	}
+
+	override function create()
+	{
+		super.create();
+
+		var bgColor = 0xFFCC99;
+
+		if (Compiler.getDefine('RESULTS_DONT_CHANGE_BGCOLOR') != '1')
+		{
+			if (characterManager.id == TJ)
+				bgColor = 0x99CCFF;
+		}
+
+		add(new FlxSprite().makeGraphic(FlxG.width, FlxG.height, bgColor));
 	}
 }
