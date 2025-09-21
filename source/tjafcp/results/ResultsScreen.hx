48169600e8b7a7ed4aca2f91eb9c43301848f66a
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-20 22:10:20 -0500

    % + percentNumber rise

--- source/tjafcp/results/ResultsScreen.hx
+++ source/tjafcp/results/ResultsScreen.hx
@@ -4,12 +4,14 @@ import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxSpriteGroup;
+import flixel.math.FlxMath;
 import flixel.util.FlxColor;
 import haxe.macro.Compiler;
 import tjafcp.results.ResultsCharacter.ResultsCharacterID;
 
 class ResultsScreen extends FlxState
 {
+	public var stats:ResultsStats;
 	public var score:ResultsScore;
 
 	public var characterManager:ResultsCharacter;
@@ -25,6 +27,7 @@ class ResultsScreen extends FlxState
 		}
 
 		score = new ResultsScore(stats.good, stats.total);
+		this.stats = stats;
 		characterManager = new ResultsCharacter(character);
 	}
 
@@ -45,6 +48,11 @@ class ResultsScreen extends FlxState
 		numbers = new FlxSpriteGroup();
 		add(numbers);
 
+		percent.scale.set(.5, .5);
+		percent.updateHitbox();
+		percent.animation.play('%');
+		percent.screenCenter();
+
 		for (i in 0...3)
 		{
 			var number = new ResultsFont();
@@ -59,11 +67,38 @@ class ResultsScreen extends FlxState
 				number.x -= number.width + 16;
 
 			if (i == 2)
+			{
 				number.x += number.width + 16;
+				percent.x = number.x + percent.width + 16;
+			}
 
 			numbers.add(number);
 		}
+
+		add(percent);
 	}
 
 	public var numbers:FlxSpriteGroup;
+
+	public var percent:ResultsFont = new ResultsFont('symbols');
+
+	public var percentNumber:Float = 0;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		percentNumber = FlxMath.lerp(percentNumber, (stats.good / stats.total) * 100, 1 / 1000);
+
+		for (number in numbers)
+		{
+			var ppercentNumber = Std.int(percentNumber);
+			if (ppercentNumber >= 100 && number.ID == 2)
+				number.animation.play(Std.string(ppercentNumber).charAt(3));
+			if (ppercentNumber >= 10 && number.ID == 1)
+				number.animation.play(Std.string(ppercentNumber).charAt(2));
+			if (number.ID == 0)
+				number.animation.play(Std.string(ppercentNumber).charAt(1));
+		}
+	}
 }
