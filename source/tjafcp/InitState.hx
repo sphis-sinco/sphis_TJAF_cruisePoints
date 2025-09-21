b66a0713ae67321a301f5a6fecd6deaa8fe727d9
Sphis-Sinco <sinconsistencia@gmail.com>
2025-09-20 21:49:28 -0500

    more results defines

--- source/tjafcp/InitState.hx
+++ source/tjafcp/InitState.hx
@@ -3,7 +3,9 @@ package tjafcp;
 import flixel.FlxG;
 import flixel.FlxState;
 import haxe.macro.Compiler;
+import tjafcp.results.ResultsCharacter.ResultsCharacterID;
 import tjafcp.results.ResultsScreen;
+import tjafcp.results.ResultsStats;
 
 class InitState extends FlxState
 {
@@ -13,7 +15,23 @@ class InitState extends FlxState
 
 		if (Compiler.getDefine('RESULTS_SCREEN') == '1')
 		{
-			FlxG.switchState(() -> new ResultsScreen());
+			var char = ResultsCharacterID.NONE;
+
+			if (Compiler.getDefine('RESULTS_CHAR_TJ') == '1')
+				char = ResultsCharacterID.TJ;
+
+			if (Compiler.getDefine('RESULTS_EXCELLENT') == '1')
+				FlxG.switchState(() -> new ResultsScreen(new ResultsStats(5, 5), char));
+			else if (Compiler.getDefine('RESULTS_GREAT') == '1')
+				FlxG.switchState(() -> new ResultsScreen(new ResultsStats(4, 5), char));
+			else if (Compiler.getDefine('RESULTS_GOOD') == '1')
+				FlxG.switchState(() -> new ResultsScreen(new ResultsStats(3, 5), char));
+			else if (Compiler.getDefine('RESULTS_OKAY') == '1')
+				FlxG.switchState(() -> new ResultsScreen(new ResultsStats(2, 5), char));
+			else if (Compiler.getDefine('RESULTS_BAD') == '1')
+				FlxG.switchState(() -> new ResultsScreen(new ResultsStats(1, 5), char));
+			else
+				FlxG.switchState(() -> new ResultsScreen(null, char));
 		}
 		else {}
 	}
